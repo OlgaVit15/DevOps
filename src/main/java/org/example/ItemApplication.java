e892db03add632fed85a64b622b0c591577c9e23
OlgaVit15 <volgavit15@mail.ru>
2025-05-14 09:49:45 +0300

    Test

--- src/main/java/org/example/ItemApplication.java
+++ src/main/java/org/example/ItemApplication.java
@@ -7,7 +7,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "org.example")
 public class ItemApplication {
+
     public static void main(String[] args) {
+
         SpringApplication.run(ItemApplication.class, args);
     }
 }
